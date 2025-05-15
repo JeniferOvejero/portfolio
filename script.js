b0f66c23208b263234691dc4c90bfab7d05ccd37
Jenifer Ovejero <jenifersofiaovejero@gmail.com>
2025-05-15 16:02:21 -0300

    carrossel base

---
+++ script.js
@@ -0,0 +1,17 @@
+const carrossel = document.getElementById('carrossel-slides');
+const itemLargura = 450 + 32;
+
+let index = 0;
+
+function moverCarrossel(direcao) {
+    const totalItens = carrossel.children.length;
+    const visiveis = Math.floor(carrossel.parentElement.offsetWidth / itemLargura);
+
+    index += direcao;
+
+    if (index < 0) index = 0;
+    if (index > totalItens - visiveis) index = totalItens - visiveis;
+
+    const deslocamento = index * itemLargura;
+    carrossel.style.transform = `translateX(-${deslocamento}px)`;
+}
