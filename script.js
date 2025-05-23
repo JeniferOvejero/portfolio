8f5c2d619fdb95d4ca5a8377b4bb7fe0e0ebb10a
Jenifer Ovejero <jenifersofiaovejero@gmail.com>
2025-05-23 16:17:03 -0300

    habilidades

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const carrossel = document.getElementById('carrossel-slides');
 const carregandoItem = document.getElementById("itens-carregando");
+const listaHabilidades = document.getElementById("lista-habilidades");
 const itemLargura = 700 + 48;
+
+ const techIcons = {
+  figma: "https://profilinator.rishav.dev/skills-assets/figma-icon.svg",
+  sass: "https://profilinator.rishav.dev/skills-assets/sass-original.svg",
+  html: "https://profilinator.rishav.dev/skills-assets/html5-original-wordmark.svg",
+  css: "https://profilinator.rishav.dev/skills-assets/css3-original-wordmark.svg",
+  javascript: "https://profilinator.rishav.dev/skills-assets/javascript-original.svg",
+  nodejs: "https://profilinator.rishav.dev/skills-assets/nodejs-original-wordmark.svg",
+  react: "https://profilinator.rishav.dev/skills-assets/react-original-wordmark.svg",
+  python: "https://profilinator.rishav.dev/skills-assets/python-original.svg",
+  c: "https://profilinator.rishav.dev/skills-assets/c-original.svg"
+};
 
 let index = 0;
 
@@ -18,16 +31,6 @@ function moverCarrossel(direcao) {
 }
 
  // Carregar dados do Google Sheets via Web App
-  const techIcons = {
-  figma: "https://profilinator.rishav.dev/skills-assets/figma-icon.svg",
-  sass: "https://profilinator.rishav.dev/skills-assets/sass-original.svg",
-  html: "https://profilinator.rishav.dev/skills-assets/html5-original-wordmark.svg",
-  css: "https://profilinator.rishav.dev/skills-assets/css3-original-wordmark.svg",
-  javascript: "https://profilinator.rishav.dev/skills-assets/javascript-original.svg",
-  nodejs: "https://profilinator.rishav.dev/skills-assets/nodejs-original-wordmark.svg",
-  react: "https://profilinator.rishav.dev/skills-assets/react-original-wordmark.svg",
-  python: "https://profilinator.rishav.dev/skills-assets/python-original.svg"
-};
 
 fetch("https://script.google.com/macros/s/AKfycbxFJ1GHzO_tb0AQUE1aYXp7ha8Lz1hCkD_u0fe0J7OpoZQTHnhqIZ9HLi7ZJYV8c6idmA/exec")
   .then(response => response.json())
@@ -62,3 +65,20 @@ fetch("https://script.google.com/macros/s/AKfycbxFJ1GHzO_tb0AQUE1aYXp7ha8Lz1hCkD
   .catch(error => {
     console.error("Erro ao carregar os dados:", error);
   });
+
+function preencherHabilidades(){
+  for (const [nome, url] of Object.entries(techIcons)){
+    const div = document.createElement("div");
+    div.classList.add("item-habilidade");
+
+    const img = document.createElement("img");
+    img.classList.add("habilidade");
+    img.src = url;
+    img.alt = nome;
+
+    div.appendChild(img);
+    listaHabilidades.appendChild(div);
+  }
+  }
+
+  preencherHabilidades();
